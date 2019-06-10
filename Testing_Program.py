0d0769da938dae8f3b7d43918366622828a15094
Riley Raso <rileyraso@w134-87-174-218.wireless.uvic.ca>
2019-06-10 11:35:42 -0700

    Added Tests for the function "Check_If_In_List"

--- Testing_Program.py
+++ Testing_Program.py
@@ -14,6 +14,56 @@ class TestEncode_And_Decode(unittest.TestCase):
 		#assert
 		self.assertEqual(Global_List, ["Day", "Hello"])
 
-if __name__ == '__main__':
-	unittest.main() 
+	def test_Check_If_In_List_Correct_First_Position(self):
+		#arrange
+		Encode_And_Decode.Add_To_Word_List("Hello")
+		Encode_And_Decode.Add_To_Word_List("Day")
 
+		#act
+		List_Index = Encode_And_Decode.Check_If_In_List("Day")
+
+		#assert
+		self.assertEqual(List_Index, 0)
+
+	def test_Check_If_In_List_Correct_Middle_Position(self):
+		#arrange
+		Encode_And_Decode.Add_To_Word_List("Hello")
+		Encode_And_Decode.Add_To_Word_List("Day")
+		Encode_And_Decode.Add_To_Word_List("Job")
+		Encode_And_Decode.Add_To_Word_List("Birthday")
+
+		#act
+		List_Index = Encode_And_Decode.Check_If_In_List("Day")
+
+		#assert
+		self.assertEqual(List_Index, 2)
+
+	def test_Check_If_In_List_Correct_Last_Position(self):
+		#arrange
+		Encode_And_Decode.Add_To_Word_List("Hello")
+		Encode_And_Decode.Add_To_Word_List("Day")
+		Encode_And_Decode.Add_To_Word_List("Job")
+		Encode_And_Decode.Add_To_Word_List("Birthday")
+
+		#act
+		List_Index = Encode_And_Decode.Check_If_In_List("Hello")
+
+		#assert
+		self.assertEqual(List_Index, 3)
+
+	def test_Check_If_In_List_Not_In_List(self):
+		#arrange
+		Encode_And_Decode.Add_To_Word_List("Hello")
+		Encode_And_Decode.Add_To_Word_List("Day")
+		Encode_And_Decode.Add_To_Word_List("Job")
+		Encode_And_Decode.Add_To_Word_List("Birthday")
+
+		#act
+		List_Index = Encode_And_Decode.Check_If_In_List("Name")
+
+		#assert
+		self.assertEqual(List_Index, -1)
+
+
+if __name__ == '__main__':
+	unittest.main()
